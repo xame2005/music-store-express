1e20942bae19e460399a99ad26dee1626ec52478
Xame2005 <xame2005@gmail.com>
2022-05-04 22:36:55 -0500

    [CREATED] DB connection settings and drivers

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const connection = require("./db");
+const app = express();
+
+connection()
+
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`Listening on port ${port}`));
