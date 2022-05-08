05976e1278ae92ca528f3ea411260728d6779b22
Xame2005 <xame2005@gmail.com>
2022-05-07 19:44:49 -0500

    [CREATED] user routes tested

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const {User, validate} = require('../models/user');
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcrypt');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
+const validateObjectId = require('../middleware/validObjectId');
 
 //create user
 router.post('/', async (req, res) => {
@@ -24,4 +27,38 @@ router.post('/', async (req, res) => {
     res.send(newUser);
 }
 );
+
+//get all users
+router.get('/', admin,  async (req, res) => {
+    const users = await User.find().select('-password -__v');
+    res.status(200).send(users);
+}
+);
+
+//get user by id
+router.get('/:id', [auth, validateObjectId], async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password -__v');
+    if (!user) return res.status(404).send('The user with the given ID was not found.');
+    res.send(user);
+}
+);
+
+//update user by id
+router.put('/:id', [auth, validateObjectId], async (req, res) => {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, { new: true }).select('-password -__v');
+    res.status(200).send(user);
+}
+);
+
+//delete user by id
+router.delete('/:id', [auth, validateObjectId], async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send('The user with the given ID was not found.');
+    res.send(user);
+}
+);
+    
+
 module.exports = router;
