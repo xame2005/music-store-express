78762774f314ed504bce7b9e838b23f887e65fcd
Xame2005 <xame2005@gmail.com>
2022-05-09 18:36:14 -0500

    [CREATED] playlist routes and index.js integration

--- routes/playlists.js
+++ routes/playlists.js
@@ -46,4 +46,88 @@ router.put('/edit/:id', auth, validObjectId, async (req, res) => {
 }
 );
 
+//add song to playlist
+router.put("/add-song", auth, async(req, res) => {
+    const schema = Joi.object({
+        playlistId: Joi.string().required(),
+        songId: Joi.string().required(),
+    });
+    const { error } = await schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findById(req.user._id);
+    const playlist = await Playlist.findById(req.body.playlistId);
+    if(!user._id.equals(playlist.user)) return res.status(401).send('You are not authorized to edit this playlist.');
+
+    if(playlist.songs.indexOf(req.body.songId)===-1){
+        playlist.songs.push(req.body.songId);
+    };
+    await playlist.save();
+    res.status(200).send({message: "Song added successfully to playlist", playlist});
+})
+
+//remove song from playlist
+router.put("/remove-song", auth, async(req, res) => {
+    const schema = Joi.object({
+        playlistId: Joi.string().required(),
+        songId: Joi.string().required(),
+    });
+    const { error } = await schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findById(req.user._id);
+    const playlist = await Playlist.findById(req.body.playlistId);
+    if(!user._id.equals(playlist.user)) return res.status(401).send('You are not authorized to edit this playlist.');
+
+    const index = playlist.songs.indexOf(req.body.songId);
+    playlist.songs.splice(index, 1);
+    await playlist.save();
+    res.status(200).send({message: "Song removed successfully from playlist", playlist});
+})
+
+//user favourite playlist
+router.get("/favourite", auth, async (req, res) => {
+    const user = await User.findById(req.user._id);
+    const playlists = await Playlist.find({_id: {$in: user.favouritePlaylists}});
+    res.status(200).send(playlists);
+})
+
+//get random playlist
+router.get("/random", async (req, res) => {
+    const playlists = await Playlist.aggregate([{$sample: {size: 9}}]);
+    res.status(200).send(playlists);
+}
+);
+
+//get playlist by id and songs
+router.get("/:id", auth, validObjectId, async (req, res) => {
+    const playlist = await Playlist.findById(req.params.id);
+    if(!playlist) return res.status(401).send('Playlist does not exist.');
+
+    const songs = await Song.find({_id: {$in: playlist.songs}});
+    res.status(200).send({playlist, songs});
+}
+);
+
+//get all playlists
+router.get("/", auth, async (req, res) => {
+    const playlists = await Playlist.find({user: req.user._id});
+    res.status(200).send(playlists);
+}
+);
+
+//delete playlist
+router.delete("/:id",[validObjectId, auth], async (req, res) => {
+    const user = await User.findById(req.user._id);
+    const playlist = await Playlist.findById(req.params.id);
+    if(!user._id.equals(playlist.user)) return res.status(401).send('You are not authorized to delete this playlist.');
+
+    const index = user.playlists.indexOf(playlist._id);
+    user.playlists.splice(index, 1);
+    await user.save();
+    await playlist.remove();
+    res.status(200).send({message: "Playlist deleted successfully"});
+}
+);
+
 module.exports = router;
